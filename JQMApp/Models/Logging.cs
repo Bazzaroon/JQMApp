7bfb6d354c8af6a1f6c4489b9e6e6f1ecc4688e4
Bazzaroon <barry.tait@gmail.com>
2016-01-15 13:23:34 +0000

    Added logging

---
+++ JQMApp/Models/Logging.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace JQMApp.Models
+{
+    static public class Logging
+    {
+        private static string path = Directory.GetCurrentDirectory() + "log.txt";
+
+        public static void log(string msg)
+        {
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["weddingconnection"].ToString();
+            var cmd = new SqlCommand();
+            string query = "insert into Error (ErrorDate, Description) values (@errordate, @description)";
+            cmd.CommandText = query;
+
+            cmd.Parameters.Add(new SqlParameter("@errordate", DateTime.Now ));
+            cmd.Parameters.Add(new SqlParameter("@description", msg));
+
+            cmd.Connection = new SqlConnection(connStr);
+
+            try
+            {
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+            }
+            catch (SqlException sqlException)
+            {
+                cmd.Connection.Close();
+            }
+
+        }
+
+        }
+
+ }
